40afde91b7d1428a1c6e61958821245c63afeb11
Vipul Mashruwala <mashruwala.vipul@gmail.com>
2022-11-11 11:08:56 +0530

    second commmit

--- testapp/serializers.py
+++ testapp/serializers.py
@@ -1,40 +1,26 @@
 from rest_framework import serializers
 from .models import Student
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['name','roll','city','marks']
+        # read_only_fields = ['roll']
+        extra_kwargs = {
+            'name' : {
+                'required': True
+            },
+            'roll' : {
+                'required': True
+            },
+            'city' : {
+                'required' : True
+            },
+            'marks' : {
+                'required' : True
+            }
+        }
 
-def check_city(value):
-    city_list = ['Bihar', 'Goa', 'Vadodara', 'Mumbai', 'Shirpur', 'Delhi','Pune','Surat','Burhanpur']
-    if value not in city_list:
-        raise serializers.ValidationError('check entered city')
 
-
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 20)
-    roll = serializers.IntegerField()
-    city = serializers.CharField(max_length = 20, validators = [check_city])
-    marks = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name',instance.name)
-        instance.roll = validated_data.get('roll',instance.roll)
-        instance.city = validated_data.get('city',instance.city)
-        instance.marks = validated_data.get('marks',instance.marks)
-        instance.save()
-        return instance
-
-    ### Validation
-    def validate_roll(self, value):
-        if value >= 200:
-            raise serializers.ValidationError("Seat Full")
-        return value
-
-    def validate(self, data):
-        marks = data.get('marks')
-        if marks < 33:
-            raise serializers.ValidationError("only pass student data can be inserted")
-        return data
-
+  
     
